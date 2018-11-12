150cf052c8b004b15553897be437fe74abde4ceb
Jesse Nickles <jesse@littlebizzy.com>
2018-11-11 20:49:37 -0800

    Update server-status.php

--- server-status.php
+++ server-status.php
@@ -3,7 +3,7 @@
 Plugin Name: Server Status
 Plugin URI: https://www.littlebizzy.com/plugins/server-status
 Description: Useful statistics about the server OS, CPU, RAM, load average, memory usage, IP address, hostname, timezone, disk space, PHP, MySQL, caches, etc.
-Version: 1.3.2
+Version: 1.4.0
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
@@ -34,7 +34,7 @@ if (!is_admin())
 // Plugin constants
 define('SVRSTS_FILE', __FILE__);
 define('SVRSTS_PATH', dirname(SVRSTS_FILE));
-define('SVRSTS_VERSION', '1.3.2');
+define('SVRSTS_VERSION', '1.4.0');
 define('SVRSTS_REFRESH', '30'); // seconds
 
 
