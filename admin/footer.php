45afb9e0b2e72b416e615ca48b8329bfc2afc5b8
Jesse Nickles <jesse@littlebizzy.com>
2019-02-16 04:46:16 +0700

    Update footer.php

--- admin/footer.php
+++ admin/footer.php
@@ -22,7 +22,7 @@ class SVRSTS_Admin_Footer {
 		$memory = SVRSTS_Core_Status::memory_usage(false);
 
 		// Concatenate and return
-		return $text.' | Memory Usage : '.esc_html($memory['usage_ini']).' ('.esc_html($memory['percent']).'%) of '.esc_html($memory['limit_ini']).' | '.esc_html(SVRSTS_Core_Status::server_ip_address()).' ('.esc_html(gethostname()).')';
+		return $text.'Server: '.esc_html(SVRSTS_Core_Status::server_ip_address()).' ('.esc_html(gethostname()).') - Memory: '.esc_html($memory['usage_ini']).' ('.esc_html($memory['percent']).'%) of '.esc_html($memory['limit_ini']).' - Disk: '.esc_html($disk_space['used']).' ('.esc_html($disk_space['uper']).'%) of '.$disk_space['total'].'';
 	}
 
 
